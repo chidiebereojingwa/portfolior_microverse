be9a8fd23b7ffda1c70031d8336ac3021733d07c
chidiebereojingwa <ojingwaoji@gmail.com>
2022-03-09 22:52:37 +0100

    Modified Script.js

--- js/script.js
+++ js/script.js
@@ -1,45 +1,45 @@
-import mydata from "../mydata";
+import mydata from '../mydata.js';
 
 console.log(mydata);
 
-const menu = document.querySelector("#menu");
-const closeIcon = document.querySelector(".close-icon");
-const mobileMenu = document.querySelector("#mobile-menu");
-const sandwichIcon = document.querySelector(".sandwich");
-const menuItem = document.querySelectorAll(".menu-list-item");
-const header = document.querySelector("header");
+const menu = document.querySelector('#menu');
+const closeIcon = document.querySelector('.close-icon');
+const mobileMenu = document.querySelector('#mobile-menu');
+const sandwichIcon = document.querySelector('.sandwich');
+const menuItem = document.querySelectorAll('.menu-list-item');
+const header = document.querySelector('header');
 
 function clickSandwichIcon() {
-  document.body.style.overflowY = "hidden";
-  header.style.cssText = "height: 100vh; background-color: #3c3a39;";
-  menu.style.display = "none";
-  menu.style.position = "static";
-  mobileMenu.style.display = "block";
+  document.body.style.overflowY = 'hidden';
+  header.style.cssText = 'height: 100vh; background-color: #3c3a39;';
+  menu.style.display = 'none';
+  menu.style.position = 'static';
+  mobileMenu.style.display = 'block';
 }
 
-sandwichIcon.addEventListener("click", clickSandwichIcon);
+sandwichIcon.addEventListener('click', clickSandwichIcon);
 
 function closeMenu() {
-  document.body.style.overflowY = "visible";
-  header.style.cssText = "height: 72px; background-color: #3c3a39;";
-  menu.style.display = "flex";
-  mobileMenu.style.display = "none";
+  document.body.style.overflowY = 'visible';
+  header.style.cssText = 'height: 72px; background-color: #3c3a39;';
+  menu.style.display = 'flex';
+  mobileMenu.style.display = 'none';
 }
 
-closeIcon.addEventListener("click", closeMenu);
+closeIcon.addEventListener('click', closeMenu);
 
 menuItem.forEach((item) => {
-  item.addEventListener("click", closeMenu);
+  item.addEventListener('click', closeMenu);
 });
 
 // pop window
 
-const myWork = document.querySelector("#my-work");
-const cardsWrapper = document.querySelector(".cards-wrapper");
+const myWork = document.querySelector('#my-work');
+const cardsWrapper = document.querySelector('.cards-wrapper');
 
 // My work
 
-let itemElement = "";
+let itemElement = '';
 mydata[0].technologies.forEach((tech) => {
   itemElement += `<li><a href="#">${tech}</a></li>`;
 });
@@ -87,7 +87,7 @@ for (let i = 1; i < mydata.length; i += 1) {
   </div>
 `;
 
-cardsWrapper.appendChild(cardDiv);
+  cardsWrapper.appendChild(cardDiv);
   const currentCard = document.querySelector(`.card.card-${i}`);
   currentCard.style.background = ` linear-gradient(
     180.45deg,
@@ -117,3 +117,82 @@ for (let i = 1; i < mydata.length; i += 1) {
     url('${mydata[i].featuredImage}')`;
   });
 }
+
+const workButtons = document.querySelectorAll('#work-btn');
+
+const createModal = (i) => {
+  let techStr = '';
+  mydata[i].technologies.forEach((tech) => {
+    techStr += `<li><a href="">${tech}</a></li>`;
+  });
+  const modal = `
+  <div class="modal-content">
+          <div class="modal-title">
+            <h2>${mydata[i].name}</h2>
+            <div id="close-works-modal">
+              <img
+                src="./assets/icons/close-popup-menu.png"
+                alt="close modal"
+              />
+            </div>
+          </div>
+          <ul class="tech-tags">${techStr}</ul>
+          <div class="modal-main">
+            <div class="modal-img">
+              <img src="${mydata[i].featuredImage}" alt="work" />
+            </div>
+            <div class="modal-info">
+              <div class="modal-right">
+                <p>${mydata[i].description}</p>
+                <div class="modal-btns">
+                  <button id="btn-1" class="btn btn-main" type="button">
+                  <a href="${mydata[i].urlLive}" class="modal-links">
+                  <span>
+                    See Live
+                  </span>
+                  <img
+                    class="icon"
+                    src="./assets/icons/see-live.png"
+                    alt="see live"
+                /></a>
+                  </button>
+                  <button id="btn-2" class="btn btn-main" type="button">
+                  <a href="${mydata[i].urlRepo}" class="modal-links">
+                  <span>
+                    See Source
+                  </span>
+                  <img
+                    class="icon"
+                    src="./assets/icons/github-modal.png"
+                    alt="see source"
+                /></a>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+  `;
+  return modal;
+};
+
+const displayModal = (event) => {
+  const modalSection = document.querySelector('#work-modal');
+  const index = event.target.dataset.work;
+  const workModal = createModal(index);
+  modalSection.innerHTML = workModal;
+  modalSection.style.display = 'block';
+  const closeModalBtn = document.querySelector('#close-works-modal');
+  closeModalBtn.addEventListener('click', () => {
+    modalSection.style.display = 'none';
+  });
+  document.body.addEventListener('click', (e) => {
+    if (e.target.id === 'work-modal') {
+      modalSection.style.display = 'none';
+    }
+  });
+};
+
+workButtons.forEach((btn) => {
+  btn.addEventListener('click', displayModal);
+});
